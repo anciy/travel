9b45b995df1f754c068bbd674b9f204fd3314c15
anciy <anciyanciy@163.com>
2021-04-24 22:38:15 +0800

    city-axios

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,17 +1,40 @@
 <template>
 <div>
     <city-header></city-header>
-    <city-hot></city-hot>
+    <city-hot :citydata="citydata"></city-hot>
 </div>    
 </template>
 <script>
 import CityHeader from "./components/cityheader"
 import CityHot from "./components/hotcity"
+import axios from "axios"
 export default {
     name:"City",
     components:{
     CityHeader,
     CityHot
+  },
+  data(){
+    return{
+      citydata:[],
+      hotcitylist:[],
+      hotapllist:[],
+      CityHot:[]
+    }
+  },
+  methods: {
+    getCityData (){
+      axios.get("/static/imooc.json").then((res)=>{
+        res=res.data.data;
+        this.hotcitylist = res.hotcitylist;
+        this.hotapllist=res.hotapllist;
+        this.citylist=res.citylist;
+        this.citydata=[this.hotcitylist,this.hotapllist,this.citylist]
+      })
+    }
+  },
+  mounted () {
+    this.getCityData ();
   }
 }
 </script>
--- src/pages/city/components/hotcity.vue
+++ src/pages/city/components/hotcity.vue
@@ -2,11 +2,11 @@
 <div style="background:	#F5F5F5;">
     <div class="messg1">热门城市</div>
     <div class="container">
-        <div v-for="item of hotlist" :key="item.id">{{item.cityname}}</div>
+        <div v-for="item of hotcitylist" :key="item.id">{{item.cityname}}</div>
     </div>
     <div class="messg1">字母排序</div>
     <div class="content">
-        <div v-for="item of hotlistapl" :key="item.id">{{item.apl}}</div>
+        <div v-for="item of hotapllist" :key="item.id">{{item.apl}}</div>
     </div>
     <div class="messg1">A</div>
     <div class="contentcity">
@@ -29,43 +29,21 @@
 <script>
 export default {
     name:"CityHot",
-    data() {
-        return{
-            hotlist:[
-                {cityname:"北京",
-                id:"01"},
-                {cityname:"上海",
-                id:"02"},
-                {cityname:"三亚",
-                id:"03"},
-                {cityname:"丽江",
-                id:"04"},
-                {cityname:"杭州",
-                id:"05"},
-                {cityname:"广州",
-                id:"06"},
-                {cityname:"成都",
-                id:"07"},
-                {cityname:"深圳",
-                id:"08"},
-                {cityname:"苏州",
-                id:"09"},
-                {cityname:"桂林",
-                id:"010"},
-                {cityname:"西安",
-                id:"011"},
-                {cityname:"厦门",
-                id:"012"}],
-            hotlistapl:[
-                {apl:"A",id:"1"},{apl:"B",id:"2"},{apl:"C",id:"3"},{apl:"D",id:"4"},
-                {apl:"E",id:"5"},{apl:"F",id:"6"},{apl:"G",id:"7"},{apl:"H",id:"8"},
-                {apl:"I",id:"9"},{apl:"J",id:"10"},{apl:"K",id:"11"},{apl:"L",id:"12"},
-                {apl:"M",id:"13"},{apl:"N",id:"14"},{apl:"O",id:"15"},{apl:"P",id:"16"},
-                {apl:"Q",id:"17"},{apl:"R",id:"18"},{apl:"S",id:"19"},{apl:"T",id:"20"},
-                {apl:"U",id:"21"},{apl:"V",id:"22"}],
-            citylist:[
-                {city:"阿克苏地区",id:"1"},{city:"阿拉尔",id:"2"},{city:"阿拉尔",id:"3"},{city:"阿拉尔",id:"4"},
-                {city:"阿拉尔",id:"5"},{city:"阿拉尔",id:"6"},{city:"阿拉尔",id:"7"},{city:"阿拉尔",id:"8"}]
+    props:{
+        citydata:Array
+    },
+    data () {
+        return {
+            hotcitylist:[],
+            hotapllist:[],
+            citylist:[]
+        }
+    },
+    watch:{
+        citydata:function () {
+            this.hotcitylist=this.citydata[0];
+            this.hotapllist=this.citydata[1];
+            this.citylist=this.citydata[2]
         }
     }
 }
